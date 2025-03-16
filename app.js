59469fed132ad4f086aeca13a3845a90d881cdb9
Pablo Castro <andres.arkan@gmail.com>
2025-03-16 05:18:29 -0300

    imprimir lista y limpiar

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+let listaHtml = document.getElementById('listaAmigos');
 
 function limpiarTexto(){
     document.getElementById('amigo').value = '';
+    while(listaHtml.firstChild)
+    {
+        listaHtml.removeChild(listaHtml.firstChild);
+    }
+}
 
+function listarAmigos(){
+    
+    for(var i = 0; i <=(amigos.length)-1;i++){
+        const li = document.createElement("li");
+        li.innerHTML = amigos[i];
+        listaHtml.appendChild(li);
+    }
 }
 
 function agregarAmigo() {
@@ -16,6 +29,7 @@ function agregarAmigo() {
         amigos.push(nombreAmigo);
         console.log(amigos);
         limpiarTexto();
+        listarAmigos();
         return;
     }
 
